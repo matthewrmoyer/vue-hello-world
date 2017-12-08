f3e0543269bad62ba6c0244347e5106d5442b40a
Unknown <matthewrmoyer@gmail.com>
2017-12-08 11:00:27 -0700

    passing data with props

--- src/components/SharingDataChild.vue
+++ src/components/SharingDataChild.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
+        <button @click="changeMyNumber">Change Number In Child Only</button>
         <p>{{ myNumber }}</p>
+        <button @click="setNumberToFive">Set Number to 5 in Parent and Child</button>
     </div>
 </template>
 
@@ -12,7 +14,21 @@
                 title: 'Sharing Data Child',
             }
         },
-        props: ['myNumber']
+        //properties set from outside the component 
+        props: {
+            'myNumber': {
+                // type: Number
+            }
+        },
+        methods: {
+            changeMyNumber() {
+                this.myNumber++
+            },
+            setNumberToFive() {
+                this.myNumber = 5
+                this.$emit('setNumberToFiveEvent', this.myNumber)
+            }
+        },
     }
 </script>
 
--- src/components/SharingDataParent.vue
+++ src/components/SharingDataParent.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
+        <p>{{ myNumber }}</p>
+        <input type="number" name="number-input" id="number-input" placeholder="type number to send to the child" @input="setMyNumber">
         <button @click="changeNumber">Change Number</button>
-        <app-sharing-data-child :my-number="myNumber"></app-sharing-data-child>
+        <app-sharing-data-child :my-number="myNumber" @setNumberToFiveEvent="myNumber=$event"></app-sharing-data-child>
     </div>
 </template>
 
@@ -13,12 +15,16 @@
         data() {
             return {
                 title: 'Sharing Data Parent',
-                myNumber: 100
+                myNumber: 100,
+                newNumber: 0,
             }
         },
         methods: {
             changeNumber() {
-                this.myNumber = 9999
+                this.myNumber = this.newNumber
+            },
+            setMyNumber(e) {
+                this.newNumber = parseInt(e.target.value)
             }
         },
         components: {
@@ -28,5 +34,7 @@
 </script>
 
 <style scoped>
-
+    #number-input {
+        width: 200px;
+    }
 </style>
